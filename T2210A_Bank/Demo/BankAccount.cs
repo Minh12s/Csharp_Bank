a9683b9962ae115258fd804d8c8f45920c652944
TNM <114908270+Minh12s@users.noreply.github.com>
2023-08-17 15:23:12 +0700

    Add files via upload

---
+++ T2210A_Bank/Demo/BankAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2210A_Bank.Demo
+{
+    public class BankAccount
+    {
+        private decimal balance;
+
+        
+        public event Action<decimal> BalanceChanged;
+
+       
+        public decimal Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (balance != value)
+                {
+                    balance = value;
+                    OnBalanceChanged(balance);
+                }
+            }
+        }
+
+        
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("so du khong du.");
+            }
+        }
+
+        
+        protected virtual void OnBalanceChanged(decimal newBalance)
+        {
+            BalanceChanged?.Invoke(newBalance);
+        }
+    }
+}
+
---
+++ T2210A_Bank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using T2210A_Bank.Demo;
+
+
+namespace T2210A_Bank
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+
+           
+            account.BalanceChanged += (newBalance) =>
+            {
+                Console.WriteLine("So du thay doi: " + newBalance);
+            };
+
+            
+            account.Deposit(1000);
+
+            
+            account.Withdraw(500);
+        }
+    }
+}
